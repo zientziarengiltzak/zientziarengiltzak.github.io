7dddd9ba721ce30bdfa6e6104be47d21ccba9c1f
Ander <ancalotoru@gmail.com>
2018-05-29 13:36:40 +0200

    Bugs fixed

---
+++ berria/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,116 @@
+var store = [{
+        "title": "Aurreko Hitzaldiak",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2016/08/01/aurreko-hitzaldiak/",
+        "teaser":null},{
+        "title": "Ciencia Y Tecnica De La Investigacion Criminal",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2016/09/13/ciencia-y-tecnica-de-la-investigacion-criminal/",
+        "teaser":null},{
+        "title": "La Cueva De Atxurra",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2016/09/26/la-cueva-de-atxurra/",
+        "teaser":null},{
+        "title": "Dislexia Ebolutiboa",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2016/10/25/dislexia-ebolutiboa/",
+        "teaser":null},{
+        "title": "Klima Aldaketa",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2016/11/28/klima-aldaketa/",
+        "teaser":null},{
+        "title": "Espeleologia",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2016/12/12/espeleologia/",
+        "teaser":null},{
+        "title": "La Dosis Importa",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/01/16/la-dosis-importa/",
+        "teaser":null},{
+        "title": "Zientzia Estropadak",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/02/20/zientzia-estropadak/",
+        "teaser":null},{
+        "title": "Mujeres Ciencia",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/03/27/mujeres-ciencia/",
+        "teaser":null},{
+        "title": "Kantabriar Mendikateko Hartzak",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/04/24/kantabriar-mendikateko-hartzak/",
+        "teaser":null},{
+        "title": "Guerra En Siria Irak",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/05/19/guerra-en-siria-irak/",
+        "teaser":null},{
+        "title": "Genomaren Sekretuak Argitu Nahirik",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/05/29/genomaren-sekretuak-argitu-nahirik/",
+        "teaser":null},{
+        "title": "Aspectos Sorprendentes Del Sistema Solar",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/06/19/aspectos-sorprendentes-del-sistema-solar/",
+        "teaser":null},{
+        "title": "Prevenir Y Tratar La Enfermedad De Alzheimer",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/10/02/prevenir-y-tratar-la-enfermedad-de-alzheimer/",
+        "teaser":null},{
+        "title": "Cern Una Ventana Al Universo Invisible",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/10/31/cern-una-ventana-al-universo-invisible/",
+        "teaser":null},{
+        "title": "Grafenoa",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/11/27/grafenoa/",
+        "teaser":null},{
+        "title": "Ahots Teknika",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/12/11/ahots-teknika/",
+        "teaser":null},{
+        "title": "Inteligencia Artificial",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2018/01/29/inteligencia-artificial/",
+        "teaser":null},{
+        "title": "Nola Gidatzen Dira Espaziontziak",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2018/02/16/nola-gidatzen-dira-espaziontziak/",
+        "teaser":null},{
+        "title": "Gliomen Modelizazioa",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2018/03/19/gliomen-modelizazioa/",
+        "teaser":null},{
+        "title": "Buruko Eta Lepoko Minbizia",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2018/04/23/buruko-eta-lepoko-minbizia/",
+        "teaser":null},{
+        "title": "Sorkuntza Eta Zientzia Sukalderako Bidelagunak",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2018/05/18/sorkuntza-eta-zientzia-sukalderako-bidelagunak/",
+        "teaser":null},{
+        "title": "Ibaien Kontserbazioa",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2018/05/28/ibaien-kontserbazioa/",
+        "teaser":null}]
