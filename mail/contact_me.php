cb3b91e2573a1c270b408e72a4696630967fd913
zientziarengiltzak <zientziarengiltzak@gmail.com>
2025-03-07 18:16:34 +0100

    Email helbide aldaketa

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'borja.calvo@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'zientziarengiltzak@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "ZGe:  $name";
 $email_body = "ZGe webgunetik mezu bat jaso dugu.\n\n"."Hauek dira xehetasunak:\n\nIzena: $name\n\nE-posta: $email_address\n\nMezua:\n$message";
 $headers = "From: noreply@zientziarengiltzak.org\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
